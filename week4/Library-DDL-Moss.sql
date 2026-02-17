0c35f4fba7ea32669d56612b1e29e75d6b06bff5
Merrick <merrickm@live.com>
2026-02-17 18:19:09 -0500

    adding schema references.

--- week4/Library-DDL-Moss.sql
+++ week4/Library-DDL-Moss.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS library.BOOK (
     Title VARCHAR(100),
     Publisher_name VARCHAR(100),
     PRIMARY KEY (Book_id),
-    FOREIGN KEY (Publisher_name) REFERENCES PUBLISHER(Name)
+    FOREIGN KEY (Publisher_name) REFERENCES library.PUBLISHER(Name)
 );
 
 CREATE TABLE IF NOT EXISTS library.LIBRARY_BRANCH (
@@ -34,15 +34,15 @@ CREATE TABLE IF NOT EXISTS library.BOOK_AUTHORS (
     Book_id INT,
     Author_name VARCHAR(100),
     PRIMARY KEY (Author_name),
-    FOREIGN KEY (Book_id) REFERENCES BOOK(Book_id)
+    FOREIGN KEY (Book_id) REFERENCES library.BOOK(Book_id)
 );
 
 CREATE TABLE IF NOT EXISTS library.BOOK_COPIES (
     Book_id INT,
     Branch_id INT,
     No_of_copies INT,
-    FOREIGN KEY (Book_id) REFERENCES BOOK(Book_id),
-    FOREIGN KEY (Branch_id) REFERENCES LIBRARY_BRANCH(Branch_id)
+    FOREIGN KEY (Book_id) REFERENCES library.BOOK(Book_id),
+    FOREIGN KEY (Branch_id) REFERENCES library.LIBRARY_BRANCH(Branch_id)
 );
 
 CREATE TABLE IF NOT EXISTS library.BOOK_LOANS (
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS library.BOOK_LOANS (
     Card_no INT,
     Date_out DATE,
     Due_date DATE,
-    FOREIGN KEY (Book_id) REFERENCES BOOK(Book_id),
-    FOREIGN KEY (Branch_id) REFERENCES LIBRARY_BRANCH(Branch_id),
-    FOREIGN KEY (Card_no) REFERENCES BORROWER(Card_no)
+    FOREIGN KEY (Book_id) REFERENCES library.BOOK(Book_id),
+    FOREIGN KEY (Branch_id) REFERENCES library.LIBRARY_BRANCH(Branch_id),
+    FOREIGN KEY (Card_no) REFERENCES library.BORROWER(Card_no)
 );
